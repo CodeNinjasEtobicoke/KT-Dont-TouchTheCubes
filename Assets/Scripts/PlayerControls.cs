904cbe8bfc30a50fa7f25c28f28fa3dc4d8b252a
CodeNinjasEtobicoke <107214218+CodeNinjasEtobicoke@users.noreply.github.com>
2024-07-02 16:37:32 -0400

    on step 18

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -7,12 +7,12 @@ public class PlayerControls : MonoBehaviour
 {
     [Header("Rigidbody")]
     public Rigidbody rb;
-
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         Time.timeScale = 1f;
+
     }
 
     // Update is called once per frame
@@ -20,9 +20,11 @@ public class PlayerControls : MonoBehaviour
     {
         transform.rotation *= Quaternion.Euler(0, 0, 7 * Time.deltaTime);
         Time.timeScale += Time.fixedDeltaTime * 0.01f;
-        rb.velocity += transform.rotation * (Vector3.right * Input.GetAxisRaw("Horizontal") * 10f * Time.deltaTime);
-        rb.velocity += transform.rotation * (Vector3.up * Input.GetAxisRaw("Vertical") * 10f * Time.deltaTime);
-        
+        rb.velocity += transform.rotation * (Vector3.right * Input.GetAxisRaw("Horizantal") * 10f * Time.deltaTime);
+        rb.velocity += transform.rotation * (Vector3.right * Input.GetAxisRaw("Horizantal") * 10f * Time.deltaTime);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -30f, 30f),
+            transform.position.y, Mathf.Clamp(transform.position.z, -30f, 30));       
+
 
     }
 }
